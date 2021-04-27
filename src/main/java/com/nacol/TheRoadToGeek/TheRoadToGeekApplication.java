0d4696fe50765ac116cb988873b1907b73f5cfa2
Nacol <nacol@sina.com>
2021-04-27 12:26:45 +0800

    init

---
+++ src/main/java/com/nacol/TheRoadToGeek/TheRoadToGeekApplication.java
@@ -0,0 +1,13 @@
+package com.nacol.TheRoadToGeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRoadToGeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRoadToGeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nacol/TheRoadToGeek/TheRoadToGeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nacol.TheRoadToGeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRoadToGeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
