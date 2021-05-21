b9cfa8a864c735780293a31f162731502abe17a5
Nacol_01 <Nacol@sina.com>
2021-05-21 08:56:01 +0800

    Future

--- src/main/java/com/nacol/TheRoadToGeek/week_03/other/pool/FixPool.java
+++ src/main/java/com/nacol/TheRoadToGeek/week_03/other/pool/FixPool.java
@@ -3,16 +3,47 @@ package com.nacol.TheRoadToGeek.week_03.other.pool;
 import com.nacol.TheRoadToGeek.common.entity.http.HttpRequestDto;
 import com.nacol.TheRoadToGeek.common.entity.http.HttpResponseDto;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class FixPool {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService pool = Executors.newFixedThreadPool(10);
-        for (int i = 0; i < 10000; i++) {
-            Future<?> result = pool.submit(()->new TaskHandler().doTask(new HttpRequestDto()));
-            HttpResponseDto resp = (HttpResponseDto)result.get();
-            System.out.println(resp.getStatus());
+        List<Future<List<?>>> results = new ArrayList<>();
+        List<Future<?>> results2 = new ArrayList<>();
+        List<Future<?>> results3 = new ArrayList<>();
+        for (int i = 0; i < 1; i++) {
+            Future<?> r1 = pool.submit(()->new TaskHandler().doTask(new HttpRequestDto()));
+            Future<List<?>> result = pool.submit(()->new TaskHandler().doMix(new HttpRequestDto()));
+            Future<?> r2 = pool.submit(()->new TaskHandler().doTask(new HttpRequestDto()));
+            results.add(result);
+            results2.add(r1);
+            results3.add(r2);
         }
+
+        for (Future<?> future : results2) {
+            HttpResponseDto rsp = (HttpResponseDto)future.get();
+            System.out.println(rsp);
+        }
+        for (Future<?> result : results) {
+            List<HttpResponseDto> resps = (List<HttpResponseDto>)result.get();
+            for (HttpResponseDto resp : resps) {
+                System.out.println(resp.getStatus());
+            }
+        }
+        System.out.println("----------");
+        for (Future<?> future : results3) {
+            try {
+                HttpResponseDto rsp = (HttpResponseDto)future.get();
+                System.out.println(rsp);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
     }
 }
--- src/main/java/com/nacol/TheRoadToGeek/week_03/other/pool/TaskHandler.java
+++ src/main/java/com/nacol/TheRoadToGeek/week_03/other/pool/TaskHandler.java
@@ -3,6 +3,9 @@ package com.nacol.TheRoadToGeek.week_03.other.pool;
 import com.nacol.TheRoadToGeek.common.entity.http.HttpRequestDto;
 import com.nacol.TheRoadToGeek.common.entity.http.HttpResponseDto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskHandler {
 
     public HttpResponseDto doTask(HttpRequestDto httpRequestDto) {
@@ -12,6 +15,17 @@ public class TaskHandler {
     public HttpResponseDto doError(HttpRequestDto httpRequestDto) {
         int num = 1/0;
         return new HttpResponseDto().setStatus(200);
+    }
+
+    public List<HttpResponseDto> doMix(HttpRequestDto httpRequestDto) {
+        List<HttpResponseDto> resps = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            if (i == 4) {
+                resps.add(doError(httpRequestDto));
+            }
+            resps.add(doTask(httpRequestDto));
+        }
+        return resps;
     }
 
 }
